0e9c99f5760fefdfcd1003cf714d53eb19c2c56f
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2020-01-09 16:06:13 +0000

    dynamic task completed

--- Static_and_Dynamic_Task_A/specs/card_game_spec.rb
+++ Static_and_Dynamic_Task_A/specs/card_game_spec.rb
@@ -4,3 +4,22 @@ require_relative('../card')
 require_relative('../card_game')
 
 class CardGameTest < Minitest::Test
+
+  def setup
+    @card1 = Card.new('ace', 1)
+    @card2 = Card.new('clubs', 2)
+  end
+
+  def test_check_for_Ace_returns_true
+    assert_equal(true, CardGame.checkforAce(@card1))
+  end
+
+  def test_check_the_highest_card
+    assert_equal(@card2, CardGame.highest_card(@card1, @card2))
+  end
+
+  def test_check_cards_total
+    assert_equal("You have a total of 3", CardGame.cards_total([@card1, @card2]))
+  end
+
+end
